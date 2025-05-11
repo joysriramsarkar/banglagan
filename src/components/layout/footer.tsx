3e3161bed59389a09520a7303d186e264e16c767
জয়শ্রীরাম সরকার <joysriram.sarkar.56@gmail.com>
2025-05-11 08:18:24 +0000

    শুধুমাত্র ফুটারের ফন্ট ছোটো করো।

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="py-6 md:px-8 md:py-0 border-t bg-secondary/50">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground">
+    <footer className="py-4 md:px-8 md:py-0 border-t bg-secondary/50">
+      <div className="container flex flex-col items-center justify-center gap-2 md:h-20 md:flex-row">
+        <p className="text-center text-xs leading-normal text-muted-foreground px-2">
           © ২০২৫ বাংলা গান । তথ্য ও মৌলিক লেখা সর্বসাধারণের জন্য উন্মুক্ত (
           <a
             href="https://creativecommons.org/publicdomain/zero/1.0/"

cb17eca75a4f7e599cc57d34a49d049bfffe584b
জয়শ্রীরাম সরকার <joysriram.sarkar.56@gmail.com>
2025-05-11 03:35:51 +0000

    do the chunk one first

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true, // Temporarily removed
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true, // Temporarily removed
+  // },
   images: {
     remotePatterns: [
       {
